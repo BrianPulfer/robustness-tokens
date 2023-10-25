9330cd391cf47405db7a89fffa7757b4087c049d
Brian PUlfer <brianpulfer95@gmail.com>
2023-10-25 12:24:45 +0200

    Removed grad from val loop. Using tqdm.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 import numpy as np
 import torch
 import yaml
+from tqdm.auto import tqdm
 from accelerate import Accelerator
 from torch.nn.functional import mse_loss
 
@@ -16,20 +17,21 @@ from data.transforms import unnormalize
 from models import get_model
 
 
-def get_loss_and_mse(model, loader, criterion, attack_fn):
+def validation_loop(model, loader, criterion, attack_fn):
     total_loss_iden, total_loss_adv, total_mse = 0.0, 0.0, 0.0
     for batch in loader:
         batch_adv = attack_fn(model, batch)
+
         with torch.no_grad():
             target = model(batch, enable_robust=False)
             mse = mse_loss(unnormalize(batch_adv), unnormalize(batch))
 
-        loss_iden = criterion(model(batch, enable_robust=True), target).mean()
-        loss_adv = criterion(model(batch_adv, enable_robust=True), target).mean()
+            loss_iden = criterion(model(batch, enable_robust=True), target).mean()
+            loss_adv = criterion(model(batch_adv, enable_robust=True), target).mean()
 
-        total_loss_iden += loss_iden.item() * len(batch)
-        total_loss_adv += loss_adv.item() * len(batch)
-        total_mse += mse.item() * len(batch)
+            total_loss_iden += loss_iden.item() * len(batch)
+            total_loss_adv += loss_adv.item() * len(batch)
+            total_mse += mse.item() * len(batch)
     return total_loss_iden, total_loss_adv, total_mse
 
 
@@ -86,57 +88,63 @@ def main(args):
         model, optim, train_loader, val_loader, test_loader
     )
 
-    while steps < args["train"]["max_steps"]:
-        for batch in train_loader:
-            batch_adv = attack_fn(model, batch)
-            with torch.no_grad():
-                target = model(batch, enable_robust=False)
-                mse = mse_loss(unnormalize(batch_adv), unnormalize(batch))
+    with tqdm(total=args["train"]["max_steps"], desc="Training") as pbar:
+        while steps < args["train"]["max_steps"]:
+            for batch in train_loader:
+                batch_adv = attack_fn(model, batch)
+                with torch.no_grad():
+                    target = model(batch, enable_robust=False)
+                    mse = mse_loss(unnormalize(batch_adv), unnormalize(batch))
 
-            for _ in range(args["train"]["steps_per_batch"]):
-                loss_iden = criterion(model(batch, enable_robust=True), target).mean()
-                loss_adv = criterion(
-                    model(batch_adv, enable_robust=True), target
-                ).mean()
-                loss = loss_iden + loss_adv
-                optim.zero_grad()
-                accelerator.backward(loss)
-                optim.step()
+                for _ in range(args["train"]["steps_per_batch"]):
+                    loss_iden = criterion(
+                        model(batch, enable_robust=True), target
+                    ).mean()
+                    loss_adv = criterion(
+                        model(batch_adv, enable_robust=True), target
+                    ).mean()
+                    loss = loss_iden + loss_adv
+                    optim.zero_grad()
+                    accelerator.backward(loss)
+                    optim.step()
 
-            wandb.log(
-                {
-                    "Train Loss": loss.item(),
-                    "Train Image MSE": mse.item(),
-                    "Train Loss Adversarial": loss_iden.item(),
-                    "Train Loss Identity": loss_adv.item(),
-                },
-                step=steps,
-            )
-
-            if steps % args["train"]["val_every_n_steps"] == 0:
-                l_iden, l_adv, mse = get_loss_and_mse(
-                    model, val_loader, criterion, attack_fn
-                )
-                val_loss = l_iden + l_adv
                 wandb.log(
                     {
-                        "Val Loss": (l_iden + l_adv) / n_val_samples,
-                        "Val Loss Adversarial": l_iden / n_val_samples,
-                        "Val Loss Identity": l_adv / n_val_samples,
-                        "Val Image MSE": mse,
+                        "Train Loss": loss.item(),
+                        "Train Image MSE": mse.item(),
+                        "Train Loss Adversarial": loss_iden.item(),
+                        "Train Loss Identity": loss_adv.item(),
                     },
                     step=steps,
                 )
-                if val_loss < best_val_loss:
-                    torch.save(accelerator.get_state_dict(model), store_path)
 
-            steps += 1
-            if steps >= args["train"]["max_steps"]:
-                break
+                if steps % args["train"]["val_every_n_steps"] == 0:
+                    torch.cuda.empty_cache()
+                    l_iden, l_adv, mse = validation_loop(
+                        model, val_loader, criterion, attack_fn
+                    )
+                    val_loss = l_iden + l_adv
+                    wandb.log(
+                        {
+                            "Val Loss": (l_iden + l_adv) / n_val_samples,
+                            "Val Loss Adversarial": l_iden / n_val_samples,
+                            "Val Loss Identity": l_adv / n_val_samples,
+                            "Val Image MSE": mse,
+                        },
+                        step=steps,
+                    )
+                    if val_loss < best_val_loss:
+                        torch.save(accelerator.get_state_dict(model), store_path)
+
+                steps += 1
+                pbar.update(1)
+                if steps >= args["train"]["max_steps"]:
+                    break
 
     # Testing model with and without robustification
+    torch.cuda.empty_cache()
     model.load_state_dict(torch.load(store_path), map_location=accelerator.device)
-    l_iden, l_adv, mse = get_loss_and_mse(model, test_loader, criterion, attack_fn)
+    l_iden, l_adv, mse = validation_loop(model, test_loader, criterion, attack_fn)
     wandb.log(
         {
             "Test loss": (l_iden + l_adv) / n_test_samples,
