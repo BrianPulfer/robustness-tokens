6f950ffbcf6df014108f61d81941f3691955f040
Brian PUlfer <brianpulfer95@gmail.com>
2023-10-31 16:01:21 +0100

    Using different batch size depending on model.

--- configs/train/make_confs.py
+++ configs/train/make_confs.py
@@ -1,15 +1,27 @@
 import yaml
 
+# For 12GB GPUs
+model_batches_steps = [
+    ("dinov2_vits14", 32, 300),
+    ("dinov2_vitb14", 16, 300),
+    ("dinov2_vitl14", 8, 300),
+]
+
+"""
+# For 80GB GPUs
 model_batches_steps = [
     ("dinov2_vits14", 256, 300),
     ("dinov2_vitb14", 128, 300),
     ("dinov2_vitl14", 64, 300),
 ]
+"""
+
+NTOKENS = [0, 1, 10, 30]
 
 
 def main():
     for model_name, batch_size, steps in model_batches_steps:
-        for ntokens in [0, 1, 2, 10, 30]:
+        for ntokens in NTOKENS:
             d = {
                 "attack": {"eps": 0.0314, "lr": 0.01, "max_mse": 0.0001, "steps": 30},
                 "dataset": "imagenet",
@@ -22,7 +34,7 @@ def main():
                 "run_name": f"{model_name} | {ntokens} RTokens",
                 "seed": 0,
                 "train": {
-                    "batch_size": 128,
+                    "batch_size": batch_size,
                     "checkpoint_freq": 30,
                     "criterion": "CosineSimilarity",
                     "lr": 0.001,
