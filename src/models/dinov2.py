b7e1e7021406b943a621de04bdf0864a78a58c6e
Brian PUlfer <brianpulfer95@gmail.com>
2023-10-31 15:51:59 +0100

    0 rtokens trains a copy of the parameters.

--- src/models/dinov2.py
+++ src/models/dinov2.py
@@ -34,21 +34,35 @@ class DinoV2Robustifier(Module):
         self.return_cls = return_cls
         self.n_rtokens = max(0, n_rtokens)
 
-        hidden_dim = self.model.cls_token.shape[-1]
-        self.rtokens = torch.nn.Parameter(1e-2 * torch.randn(1, n_rtokens, hidden_dim))
-        # self.rtokens = torch.nn.Parameter(torch.zeros(1, n_rtokens, hidden_dim))
+        if self.n_rtokens > 0:
+            hidden_dim = self.model.cls_token.shape[-1]
+            self.rtokens = torch.nn.Parameter(
+                1e-2 * torch.randn(1, n_rtokens, hidden_dim)
+            )
+            # self.rtokens = torch.nn.Parameter(torch.zeros(1, n_rtokens, hidden_dim))
+        else:
+            self.extra_params = torch.nn.ParameterList(
+                [1e-3 * torch.randn_like(p) for p in self.model.parameters()]
+            )
+            self.added_params = False
 
     def get_trainable_parameters(self):
         if self.n_rtokens > 0:
             return [self.rtokens]
-        return self.model.parameters()
+        return self.extra_params
 
     def forward(self, x, enable_robust=None, return_cls=None, return_rtokens=False):
         b, c, w, h = x.shape
         running_cls = return_cls if return_cls is not None else self.return_cls
         running_robust = (
             enable_robust if enable_robust is not None else self.enable_robust
-        ) and self.n_rtokens > 0
+        )
+
+        if self.n_rtokens == 0:
+            if running_robust:
+                self.add_extra_params()
+            else:
+                self.remove_extra_params()
 
         # Embedding patches
         x = self.model.patch_embed(x)
@@ -57,8 +71,8 @@ class DinoV2Robustifier(Module):
         x = torch.cat((self.model.cls_token.expand(b, -1, -1), x), dim=1)
         x += self.model.interpolate_pos_encoding(x, w, h)
 
-        # Appending robust token
-        if running_robust:
+        # Appending robust tokens
+        if running_robust and self.n_rtokens > 0:
             x = torch.cat((x, self.rtokens.repeat(b, 1, 1)), dim=1)
 
         # Running blocks
@@ -69,7 +83,19 @@ class DinoV2Robustifier(Module):
         if running_cls:
             return self.model.head(x[:, 0])
 
-        if not return_rtokens and running_robust:
+        if not return_rtokens and self.n_rtokens > 0 and running_robust:
             return x[:, : -self.n_rtokens]
 
         return x
+
+    def add_extra_params(self):
+        if not self.added_params:
+            for p1, p2 in zip(self.extra_params, self.model.parameters()):
+                p2.data += p1.data
+            self.added_params = True
+
+    def remove_extra_params(self):
+        if self.added_params:
+            for p1, p2 in zip(self.extra_params, self.model.parameters()):
+                p2.data -= p1.data
+            self.added_params = False
