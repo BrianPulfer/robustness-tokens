31f61239c654eb31a5dbb25546f07913828e0cb0
Brian PUlfer <brianpulfer95@gmail.com>
2023-11-16 15:16:09 +0100

    Implemented depth-estimation with Adabins.

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,13 +1,10 @@
 from data.ade20k import get_loaders as ade20k_loaders
 from data.imagenet import get_loaders as imagenet_loaders
 from data.imagenette import get_loaders as imagenette_loaders
+from data.nyud import get_loaders as nyud_loaders
 
 
-SUPPORTED_DATASETS = [
-    "ade20k",
-    "imagenet",
-    "imagenette",
-]
+SUPPORTED_DATASETS = ["ade20k", "imagenet", "imagenette", "nyud"]
 
 
 def get_loaders_fn(dataset_name):
@@ -24,5 +21,8 @@ def get_loaders_fn(dataset_name):
 
     if dataset_name == "imagenette":
         return imagenette_loaders
+
+    if dataset_name == "nyud":
+        return nyud_loaders
 
     return None
