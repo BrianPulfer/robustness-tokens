ccd2d4f2853446816372a1e19b4e9f70c23f5615
Brian PUlfer <brianpulfer95@gmail.com>
2023-12-04 16:04:26 +0100

    Updated instructions to train segmentation models using SLURM with one node and 8 gpus

--- scripts/launch_segmentation.sh
+++ scripts/launch_segmentation.sh
@@ -1,13 +1,6 @@
-# SLURM commands
+# Single GPU
 # Training
-# sh src/mmsegmentation/tools/slurm_train.sh private-cui-gpu s_train_seg configs/eval/segmentation/dinov2_vits14_ade20k_linear_config.py # --work-dir results/dinov2_vits14/segmentation
-
-# Testing
-# sh src/mmsegmentation/tools/slurm_test.sh private-cui-gpu s_test_seg configs/eval/segmentation/dinov2_vits14_ade20k_linear_config.py
-
-# Non-SLURM
-# Training
-python src/mmsegmentation/tools/train.py configs/eval/segmentation/dinov2_vits14_ade20k_linear_config.py
+# python src/mmsegmentation/tools/train.py configs/eval/segmentation/dinov2_vits14_ade20k_linear_config.py
 # python src/mmsegmentation/tools/train.py configs/eval/segmentation/dinov2_vitb14_ade20k_linear_config.py
 # python src/mmsegmentation/tools/train.py configs/eval/segmentation/dinov2_vitl14_ade20k_linear_config.py
 # python src/mmsegmentation/tools/train.py configs/eval/segmentation/dinov2_vitg14_ade20k_linear_config.py
@@ -17,3 +10,11 @@ python src/mmsegmentation/tools/train.py configs/eval/segmentation/dinov2_vits14
 # python src/mmsegmentation/tools/test.py configs/eval/segmentation/dinov2_vits14_ade20k_linear_config.py results/dinov2_vitb14/segmentation/latest.pth
 # python src/mmsegmentation/tools/test.py configs/eval/segmentation/dinov2_vits14_ade20k_linear_config.py results/dinov2_vitl14/segmentation/latest.pth
 # python src/mmsegmentation/tools/test.py configs/eval/segmentation/dinov2_vits14_ade20k_linear_config.py results/dinov2_vitg14/segmentation/latest.pth
+
+
+# Multi-GPU
+# Training
+GPUS=8 GPUS_PER_NODE=8 SRUN_ARGS="--time 12:00:00" sh src/mmsegmentation/tools/slurm_train.sh shared-gpu vits-seg configs/eval/segmentation/dinov2_vits14_ade20k_linear_config.py
+GPUS=8 GPUS_PER_NODE=8 SRUN_ARGS="--time 12:00:00" sh src/mmsegmentation/tools/slurm_train.sh shared-gpu vitb-seg configs/eval/segmentation/dinov2_vitb14_ade20k_linear_config.py
+GPUS=8 GPUS_PER_NODE=8 SRUN_ARGS="--time 12:00:00" sh src/mmsegmentation/tools/slurm_train.sh shared-gpu vitl-seg configs/eval/segmentation/dinov2_vitl14_ade20k_linear_config.py
+GPUS=8 GPUS_PER_NODE=8 SRUN_ARGS="--time 12:00:00" sh src/mmsegmentation/tools/slurm_train.sh shared-gpu vitg-seg configs/eval/segmentation/dinov2_vitg14_ade20k_linear_config.py
