6dbb7d7a36b9b59be0d375bec037124d6f40dba8
Brian Pulfer <brianpulfer95@gmail.com>
2024-02-07 23:44:52 +0100

    Changed script name.

--- scripts/robustness_class.sh
+++ scripts/robustness_class.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # No registers, no robustness tokens
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/small.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/base.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/large.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/giant.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/small.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/base.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/large.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/giant.yaml
 
 # Registers, no robustness tokens
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/small-reg.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/base-reg.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/large-reg.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/giant-reg.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/small-reg.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/base-reg.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/large-reg.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/giant-reg.yaml
 
 # No registers, Robustness tokens
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/small-rob.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/base-rob.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/large-rob.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/giant-rob.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/small-rob.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/base-rob.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/large-rob.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/giant-rob.yaml
 
 # Registers and Robustness tokens
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/small-reg-rob.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/base-reg-rob.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/large-reg-rob.yaml
-PYTHONPATH=src python src/robustness/class.py --config configs/robustness/classification/giant-reg-rob.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/small-reg-rob.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/base-reg-rob.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/large-reg-rob.yaml
+PYTHONPATH=src python src/robustness/classification.py --config configs/robustness/classification/giant-reg-rob.yaml
